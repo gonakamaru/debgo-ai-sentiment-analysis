3e6b6276ea4014cd6789db1932f51ff9cd4a0d71
Go Nakamaru <gonakamaru@gmail.com>
2025-11-17 16:47:53 +0900

    add baseline setup for dev (#3)

---
+++ sentiment.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# ==============================================
+# AI Sentiment Analyzer
+# Last experimented: 2025-11-17
+# Platform: M1 Mac
+# ==============================================
+#
+# Make it positive
+# ==============================================
+
+
+def main():
+    print("hello world")
+
+
+if __name__ == "__main__":
+    main()
